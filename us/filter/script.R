2f27db4dbac135c9ae4b34e60af21f7ea990f736
Trung-Anh Nguyen <trunganh1410@gmail.com>
2022-01-24 02:44:12 -0800

    added descriptive analysis

--- us/filter/script.R
+++ us/filter/script.R
@@ -2,11 +2,11 @@ library(dplyr)
 
 arrests <- read.csv("../clean/output/arrests.csv.gz", sep = "|")
 arrests <- arrests %>% filter(aor != "", apprehension_landmark != "DO NOT USE")
-write.csv(arrests, file = gzfile("../export/output/arrests.csv.gz"))
+write.csv(arrests, file = gzfile("../export/output/arrests.csv.gz"), row.names = F)
 
 encounters <- read.csv("../clean/output/encounters.csv.gz", sep = "|")
 encounters <- encounters %>% filter(aor != "", landmark != "DO NOT USE")
-write.csv(encounters, file = gzfile("../export/output/encounters.csv.gz"))
+write.csv(encounters, file = gzfile("../export/output/encounters.csv.gz"), row.names = F)
 
 #removals <- read.csv("../clean/output/removals.csv.gz", sep = "|")
 
