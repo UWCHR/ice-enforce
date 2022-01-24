c0c614f8d0ccaf190eb2e614f6948270371897d3
Trung-Anh Nguyen <trunganh1410@gmail.com>
2022-01-23 23:18:53 -0800

    add filtered files

--- us/filter/DroppingLog.md
+++ us/filter/DroppingLog.md
@@ -3,3 +3,16 @@ Arrests:
 Originally 544,059 obs
 Dropped 2,466 "DO NOT USE" obs from `apprehension_landmark`
 Dropped 3,808 blank `aor` obs
+(What about missing values in `apprehension_landmark`?)
+
+Encounters:
+
+Originally 1,689,378 obs
+Dropped 80,421 blank `aor` obs
+Dropped 6,030 "DO NOT USE" obs from `landmark`
+(What about missing values in `landmark`?)
+
+Removals:
+
+No obs with missing `aor`, no obs with missing `removal_date`
+What about missing values in `apprehension_date` (14,235 obs)?
--- us/filter/script.R
+++ us/filter/script.R
@@ -2,5 +2,11 @@ library(dplyr)
 
 arrests <- read.csv("../clean/output/arrests.csv.gz", sep = "|")
 arrests <- arrests %>% filter(aor != "", apprehension_landmark != "DO NOT USE")
+write.csv(arrests, file = gzfile("../export/output/arrests.csv.gz"))
 
 encounters <- read.csv("../clean/output/encounters.csv.gz", sep = "|")
+encounters <- encounters %>% filter(aor != "", landmark != "DO NOT USE")
+write.csv(encounters, file = gzfile("../export/output/encounters.csv.gz"))
+
+#removals <- read.csv("../clean/output/removals.csv.gz", sep = "|")
+
