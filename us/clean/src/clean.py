9e339a5a1dd39bd0d4d591e6560c7776cbf561d9
Phil Neff <philneff@uw.edu>
2022-09-23 15:17:21 -0700

    Basic logger. Very little happening here, other values we want to clean?

--- us/clean/src/clean.py
+++ us/clean/src/clean.py
@@ -10,6 +10,7 @@ import pandas as pd
 import argparse
 import sys
 import yaml
+import logging
 if sys.version_info[0] < 3:
     raise "Must be using Python 3"
 
@@ -34,10 +35,18 @@ def mem_usage(pandas_obj):
 
 if __name__ == "__main__":
 
+    logging.basicConfig(filename='output/clean.log',
+                    filemode='a',
+                    format=f'%(asctime)s|%(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO)
+
+    logging.info('Log Start Time')
+
     args = _get_args()
 
     with open(args.dtypes, 'r') as yamlfile:
-        dtypes = yaml.load(yamlfile)
+        dtypes = yaml.safe_load(yamlfile)
 
     read_csv_opts = {'sep': '|',
                      'quotechar': '"',
@@ -46,8 +55,11 @@ if __name__ == "__main__":
 
     df = pd.read_csv(args.input, **read_csv_opts, dtype=dtypes)
 
+    logging.info(f'Input file: {args.input}')
+    logging.info(f'Rows in: {len(df)}')
+
     with open(args.cleanrules, 'r') as yamlfile:
-        cleanrules = yaml.load(yamlfile)
+        cleanrules = yaml.safe_load(yamlfile)
 
     redacted = ['birth_date']
     df = df.drop(redacted, axis=1)
@@ -65,6 +77,8 @@ if __name__ == "__main__":
                       'index': False}
 
     df.to_csv(args.output, **write_csv_opts)
-    print(f'Wrote {len(df)} records to {args.output}')
+    logging.info(f'Output file: {args.output}')
+    logging.info(f'Rows in: {len(args.output)}')
+    logging.info('Log End Time')
 
 # END.
