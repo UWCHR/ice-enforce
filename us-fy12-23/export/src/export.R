d0d03076305b75e3e20f50616d78da66e3d7db37
Phil Neff <philneff@uw.edu>
2023-05-28 20:22:23 -0700

    Simplified log names

--- us-fy12-23/export/src/export.R
+++ us-fy12-23/export/src/export.R
@@ -39,7 +39,7 @@ date_in_bounds <- arrests %>%
   filter(arrest_date >= constants$project_scope_start_date & 
          arrest_date <= constants$project_scope_end_date)
 
-log_info('Arrest record date in project scope: {nrow(date_in_bounds)}')
+log_info('Arrests in range: {nrow(date_in_bounds)}')
 
 arrests_by_method <- arrests %>% 
   mutate(arrest_method = tolower(arrest_method)) %>%
@@ -75,7 +75,7 @@ date_in_bounds <- encounters %>%
   filter(event_date >= constants$project_scope_start_date & 
          event_date <= constants$project_scope_end_date)
 
-log_info('Encounter record date in project scope: {nrow(date_in_bounds)}')
+log_info('Encounter in range: {nrow(date_in_bounds)}')
 
 encounters <- encounters %>%
   mutate(quarter = quarter(event_date, with_year = TRUE, fiscal_start = 10), 
@@ -101,7 +101,7 @@ date_in_bounds <- removals %>%
   filter(date >= constants$project_scope_start_date & 
          date <= constants$project_scope_end_date)
 
-log_info('Removal record date in project scope: {nrow(date_in_bounds)}')
+log_info('Removal in range: {nrow(date_in_bounds)}')
 
 removals_by_disp <- removals %>% 
   mutate(processing_disp = tolower(processing_disp)) %>%
